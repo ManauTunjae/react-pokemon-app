1675c1728c9f677637c6b2192e52d1c1c93e9d2b
ManauTunjae <manau.tunjae@hotmail.com>
2025-11-18 11:26:31 +0100

    Added comments to my code.

--- src/components/PokemonApplication.jsx
+++ src/components/PokemonApplication.jsx
@@ -2,10 +2,14 @@ import { useState, useEffect } from "react";
 import Pokemon from "./Pokemon";
 
 const PokemonApplication = () => {
+  // pokemons lagrar alla 151 pokemon från API:et
   const [pokemons, setPokemons] = useState([]);
+  // selectPokemonUrl lagrar URL:en till den valda pokemon
   const [selectPokemonUrl, setSelectPokemonUrl] = useState('');
+  // pokemonData lagrar information om den valda pokemon
   const [pokemonData, setPokemonData] = useState(null);
 
+  // Hämtar alla pokemon från API
   useEffect(() => {
     const getPokemons = async () => {
       let response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
@@ -14,7 +18,8 @@ const PokemonApplication = () => {
     };
     getPokemons();
   }, []);
-
+ 
+  // Funktion för att hämta informaton på den valda pokemon
   const handlePokemonData = async () => {
     let response = await fetch(selectPokemonUrl);
     let json = await response.json();
@@ -24,6 +29,8 @@ const PokemonApplication = () => {
   return (
     <div>
       <select value={selectPokemonUrl} onChange={(e) => setSelectPokemonUrl(e.target.value)}>
+        <option value="">Select a pokemon</option>
+        {/* .map() loopar igenom pokemon-arrayen och skapa <option> för varje pokemon */}
         {pokemons.map((pokemon) => (
           <option key={pokemon.name} value={pokemon.url}>
             {pokemon.name}
@@ -31,6 +38,7 @@ const PokemonApplication = () => {
         ))}
       </select>
       <button onClick={handlePokemonData}>Select Pokemon</button>
+      {/* Visar Pokemon-komponent medan pokemonData är sant */}
       {pokemonData && <Pokemon pokemon={pokemonData} />}
     </div>
   );
