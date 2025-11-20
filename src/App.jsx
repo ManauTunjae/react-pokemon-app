8f334133f892ec5d2862068d90d29073d8e31803
ManauTunjae <manau.tunjae@hotmail.com>
2025-11-20 09:14:00 +0100

    Added images and style in App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { useState } from "react";
 import "./App.css";
 import PokemonApplication from "./components/PokemonApplication";
+import pokemon from "./assets/pokemon.svg";
 
 function App() {
   // useState för att kontrollera om Pokemon-applikation ska visas.
   const [show, setShow] = useState(false);
 
   return (
-    <div>
-      <h1>Pokemon Application</h1>
+    <div className='startPage'>
       {/* Toggle knapp för att starta och avsluta applikationen. */}
-      <button onClick={() => setShow(!show)}>
+      <img style={{width: "400px"}} src={pokemon} alt="pokemon" />
+      <br />
+      <button className='startBtn' onClick={() => setShow(!show)}>
         {!show ? "Start Pokemon App" : "End Pokemon App"}
       </button>
       {/* Visar PokemonApplication-komponent medan show är sant */}
